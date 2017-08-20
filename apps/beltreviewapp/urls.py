dfb58a1586dfd797d7ebdd8af8e84c4c8b45f8bd
lex Phumirat <lexphumirat@gmail.com>
2017-08-20 09:16:31 -0700

    beltreview2

--- apps/beltreviewapp/urls.py
+++ apps/beltreviewapp/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views           # This line is new!
 urlpatterns = [
     url(r'^$', views.index),
-    
+    url(r'^addbook$', views.addbook)
+
 ]
--- apps/beltreviewapp/views.py
+++ apps/beltreviewapp/views.py
@@ -1,11 +1,14 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect, HttpResponse
+from .models import User
 from django.contrib import auth
 
 
 
 
 def index(request):
-
     return render(request , 'beltreviewapp/index.html')
+
+def addbook(request):
+    return render(request, 'beltreviewapp/books.html')
