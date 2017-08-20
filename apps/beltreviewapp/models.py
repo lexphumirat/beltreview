e214d0606f624b919c1d5b1376180cf67edb56bd
lex Phumirat <lexphumirat@gmail.com>
2017-08-20 09:21:01 -0700

    newupload

--- apps/beltreviewapp/models.py
+++ apps/beltreviewapp/models.py
@@ -3,11 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 from django.contrib import messages
 
-# class UserManager(models.Manager):
-#     User.objects.create(
-#     full_name = request.POST['full_name'],
-#     alias = request.POST['alias']
-#     )
+class UserManager(models.Manager):
+    User.objects.create(
+    full_name = request.POST['full_name'],
+    alias = request.POST['alias']
+    )
 
 
 class User(models.Model):
@@ -17,7 +17,7 @@ class User(models.Model):
     password = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now_add=True)
-    
+
 
     def __str__(self):
         return self.full_name + " " + self.email
